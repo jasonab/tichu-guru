d64c8ba7e243b86250834519605bbba2f2379c4e
Jason Bennett <jasonab@acm.org>
2026-04-03 18:26:31 -0700

    upgrades

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,12 +3,12 @@ plugins {
 }
 
 android {
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.tichuguru"
         minSdk = 30
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 4
         versionName = "1.03"
 
@@ -17,7 +17,7 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
@@ -28,16 +28,13 @@ android {
     }
 
     namespace = "com.tichuguru"
-    lint {
-        abortOnError = false
-    }
 }
 
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom:1.8.22"))
-    implementation("androidx.appcompat:appcompat:1.7.0")
-    implementation("com.google.android.material:material:1.12.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("com.google.android.material:material:1.13.0")
+    implementation("androidx.recyclerview:recyclerview:1.4.0")
 
     val roomVersion = "2.8.4"
     implementation("androidx.room:room-runtime:$roomVersion")
